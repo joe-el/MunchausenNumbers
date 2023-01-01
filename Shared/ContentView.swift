ad9981584e286b0b84a4d5a56831d10b784bf8f8
Ken <kenneth.joel@live.com>
2023-01-01 20:48:37 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kenneth Gutierrez on 1/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MunchausenNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchausenNumbersApp.swift
+//  Shared
+//
+//  Created by Kenneth Gutierrez on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MunchausenNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
